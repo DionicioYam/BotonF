b748ef81e24eda2fa8fae7c170dab81112531144
Dionicio Yam <dionicioyam@gmail.com>
2019-06-06 21:44:26 -0500

    sexto boton flotante

--- app/src/main/java/com/example/botonf/MainActivity.java
+++ app/src/main/java/com/example/botonf/MainActivity.java
@@ -164,6 +164,8 @@ public class MainActivity extends AppCompatActivity {
 
 }
 */
+/*
+
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -210,3 +212,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         morph.hide();
     }
 }
+*/
+import android.animation.Animator;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Interpolator;
+
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
+import com.github.fafaldo.fabtoolbar.widget.FABToolbarLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    boolean click=false;
+    private FABToolbarLayout morph;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionsMenu fab = findViewById(R.id.fab);
+
+    }
+    }
