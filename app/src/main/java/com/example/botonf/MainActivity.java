121e2755b7e8c90aa07492762ca088dee9ea9caf
Dionicio Yam <dionicioyam@gmail.com>
2019-06-06 20:44:28 -0500

    Cuarto boton flotante

--- app/src/main/java/com/example/botonf/MainActivity.java
+++ app/src/main/java/com/example/botonf/MainActivity.java
@@ -51,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
     }
 }
 */
+
+/* tercer boton flotante
 import android.animation.Animator;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -118,4 +120,45 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+}
+*/
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Interpolator;
+
+public class MainActivity extends AppCompatActivity {
+
+    boolean click = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setImageResource(R.drawable.es1);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                click = !click;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    Interpolator interpolador = AnimationUtils.loadInterpolator(getBaseContext(),
+                            android.R.interpolator.fast_out_slow_in);
+
+                    view.animate()
+                            .rotation(click ? 45f : 0)
+                            .setInterpolator(interpolador)
+                            .start();
+                }
+            }
+        });
+    }
+
 }
